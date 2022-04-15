06a06c372594bca3116621ffa898994d6728ea6f
Rishabh Pant <81542717+RishabhXD@users.noreply.github.com>
2022-04-15 18:01:52 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
+import Footer from "./components/Footer";
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
       <div>
         <Navbar />
         <News />
+        <Footer />
       </div>
     );
   }
