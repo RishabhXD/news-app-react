2e91b184f677051c7b4bf8f1a80b23b075b923fd
Rishabh Pant <81542717+RishabhXD@users.noreply.github.com>
2022-04-17 11:40:03 +0530

    ok

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,95 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NewsItems from "./NewsItems";
 import InfiniteScroll from "react-infinite-scroll-component";
-export class News extends Component {
-  static defaultProps = {
-    topic: "general",
-    pageSize: 20,
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [pages, setPages] = useState(1);
+  const [totalResults, setTotalResults] = useState(1);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults: 0,
-    };
-  }
-
-  async updateNews() {
-    let url = `https://newsapi.org/v2/everything?q=${this.props.topic}&apiKey=dc28211083c64f7ab654b8a96ebaec50&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const updateNews = async () => {
+    let url = `https://newsapi.org/v2/everything?q=${props.topic}&apiKey=dc28211083c64f7ab654b8a96ebaec50&page=${pages}&pageSize=${props.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-    });
-  }
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+  };
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/everything?q=${this.props.topic}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
+  const componentDidMount = async () => {
+    let url = `https://newsapi.org/v2/everything?q=${props.topic}&apiKey=${props.apiKey}&page=1&pageSize=${props.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-    });
-  }
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+  };
 
-  fetchMoreData = async () => {
-    let url = `https://newsapi.org/v2/everything?q=${this.props.topic}&apiKey=dc28211083c64f7ab654b8a96ebaec50&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const fetchMoreData = async () => {
+    let url = `https://newsapi.org/v2/everything?q=${props.topic}&apiKey=dc28211083c64f7ab654b8a96ebaec50&page=${pages}&pageSize=${props.pageSize}`;
 
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-      page: this.state.page + 1,
-    });
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setPages(pages + 1);
   };
 
-  handleNextClick = async () => {
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-  };
-  handlePrevClick = async () => {
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-  };
-
-  render() {
-    return (
-      <section className="text-gray-400 body-font bg-gray-900">
-        <div className="container p-5 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            {this.state.articles.map((element) => {
-              return (
-                <NewsItems
-                  key={element.url}
-                  title={element.title.slice(0, 50)}
-                  description={element.description.slice(0, 88)}
-                  imgUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              );
-            })}
-            <InfiniteScroll
-              dataLength={this.state.articles.length}
-              next={this.fetchMoreData}
-              hasMore={
-                this.state.articles.length !==
-                Math.ceil(100 / this.props.pageSize)
-              }
-              loader={<h4>Loading...</h4>}
-            ></InfiniteScroll>
-          </div>
+  return (
+    <section className="text-gray-400 body-font bg-gray-900">
+      <div className="container p-5 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {articles.map((element) => {
+            return (
+              <NewsItems
+                key={element.url}
+                title={element.title.slice(0, 50)}
+                description={element.description.slice(0, 88)}
+                imgUrl={element.urlToImage}
+                newsUrl={element.url}
+              />
+            );
+          })}
+          <InfiniteScroll
+            dataLength={articles.length}
+            next={fetchMoreData}
+            hasMore={articles.length !== Math.ceil(100 / props.pageSize)}
+            loader={<h4>Loading...</h4>}
+          ></InfiniteScroll>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+News.defaultProps = {
+  topic: "general",
+  pageSize: 20,
+};
 
 export default News;
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -1,43 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class NewsItems extends Component {
-  render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
-    return (
-      <div className="xl:w-1/4 md:w-1/2 p-4">
-        <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
-          <img
-            className="h-40 rounded w-full object-cover object-center mb-6"
-            src={imgUrl}
-            alt="content"
-          />
-          <h2 className="text-lg text-white font-medium title-font mb-4">
-            {title}...
-          </h2>
-          <p className="leading-relaxed text-base">{description}...</p>
-          <a
-            href={newsUrl}
-            target="_blank"
-            className="text-indigo-400 inline-flex items-center mt-3"
-            rel="noreferrer"
+const NewsItems = (props) => {
+  let { title, description, imgUrl, newsUrl } = props;
+  return (
+    <div className="xl:w-1/4 md:w-1/2 p-4">
+      <div className="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+        <img
+          className="h-40 rounded w-full object-cover object-center mb-6"
+          src={imgUrl}
+          alt="content"
+        />
+        <h2 className="text-lg text-white font-medium title-font mb-4">
+          {title}...
+        </h2>
+        <p className="leading-relaxed text-base">{description}...</p>
+        <a
+          href={newsUrl}
+          target="_blank"
+          className="text-indigo-400 inline-flex items-center mt-3"
+          rel="noreferrer"
+        >
+          Learn More
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-4 h-4 ml-2"
+            viewBox="0 0 24 24"
           >
-            Learn More
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-        </div>
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NewsItems;
